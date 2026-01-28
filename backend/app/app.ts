ecd6062469c8633ed65007a099e35db310ef6efa
Denis <dalekseenko8@gmail.com>
2026-01-28 22:35:33 +0300

    I have built the best backend architecture

---
+++ backend/app/app.ts
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
