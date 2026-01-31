3a1cfd266420e9d78a3d12988e2ed51b72e21db3
Denis <dalekseenko8@gmail.com>
2026-02-01 00:32:48 +0300

    some fixes

--- backend/modules/users/users.service.ts
+++ backend/modules/users/users.service.ts
@@ -11,6 +11,11 @@ export async function setUserOnlineService(userId: number) {
 export async function getStatusService(userId: number) {
   try {
     const lastOnline = await getStatus(userId);
+    if (!lastOnline) {
+      throw new Error("User not found");
+    }
+
+    return lastOnline;
   } catch (error) {
     console.error("Error getting user status:", error);
   }
